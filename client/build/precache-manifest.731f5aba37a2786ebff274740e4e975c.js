0af4b2ff3afadc96da525fbefb2d58418a18b37e
AdithyaPadmashali <adithya.p2001@gmail.com>
2021-08-13 14:32:31 +0530

    mess

---
+++ client/build/precache-manifest.731f5aba37a2786ebff274740e4e975c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f97448a6e05e4f2b11cb5315eaf6a04a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5027320c46c3ce6ef8d3",
+    "url": "/static/css/main.968b8aeb.chunk.css"
+  },
+  {
+    "revision": "5721770622a74ef23baa",
+    "url": "/static/js/2.eed198f6.chunk.js"
+  },
+  {
+    "revision": "5027320c46c3ce6ef8d3",
+    "url": "/static/js/main.250bda8a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
